fc19df43b1256a2aa538d180b18c593a42c80af2
chethan <pagadalachethan.kiaq@gmail.com>
2026-09-09 12:57:40 +0000

    Initial SonarQube project

---
+++ app.py
@@ -0,0 +1,13 @@
+def calculate(a, b):
+    password = "admin123"
+
+    if a > 10:
+        print("Large number")
+
+    if a > 10:
+        print("Large number")
+
+    return a + b
+
+
+print(calculate(20, 10))
